a5e19a98dc56b062a43003598ab58d35a5895d7f
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-09-12 17:47:11 +0300

    Merge Button and IconedButton components

--- src/components/blocks/Button.tsx
+++ src/components/blocks/Button.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { ButtonProps } from '../types';
 
-function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+function Button({ children, icon: Icon, ...props }: ButtonProps) {
+  return (
+    <button {...props}>
+      {Icon && <Icon aria-hidden="true" focusable="false" />}
+      {children}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,10 @@
 import Box from './blocks/Box';
 import Button from './blocks/Button';
 import Heading from './blocks/Heading';
-import IconedButton from './blocks/IconedButton';
 import Mention from './blocks/Mention';
 import Picture from './blocks/Picture';
 import Spinner from './Spinner';
 import Text from './blocks/Text';
 import TextArea from './blocks/TextArea';
 
-export { Box, Button, Heading, IconedButton, Mention, Picture, Spinner, Text, TextArea };
+export { Box, Button, Heading, Mention, Picture, Spinner, Text, TextArea };
