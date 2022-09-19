ec4b7936d18b13f327567b8fee0e76764680354d
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-09-19 22:57:55 +0300

    Read data from json file and populate UI

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,77 +2,82 @@ import React from 'react';
 import { Box, CommentBox, CommentInputForm, Modal } from '../components';
 import JuliusProfilePNG from '../assets/images/avatars/image-juliusomo.png';
 import JuliusProfileWEBP from '../assets/images/avatars/image-juliusomo.webp';
+import Data from '../data.json';
 
 function Main() {
   const [value, setValue] = React.useState(12);
   const [isOnEdit, setIsOnEdit] = React.useState(false);
   const deleteModalRef = React.useRef<HTMLDialogElement>(null);
+  const [data, setData] = React.useState(Data);
+
+  React.useEffect(() => {
+    console.log('mounted');
+    console.log(data);
+  }, []);
 
   return (
     <Box tag={'section'}>
-      <CommentBox
-        isOwn={false}
-        isOnEdit={false}
-        profileImages={{ png: JuliusProfilePNG, webp: JuliusProfileWEBP }}
-        commenter={'ramsesmiron'}
-        commentTimestamp={'1 week ago'}
-        comment={
-          "@maxblagun If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first."
-        }
-        upvoteValue={value}
-        onMinusIconClick={() => setValue(value - 1)}
-        onPlusIconClick={() => setValue(value + 1)}
-        labelID={'comment-1'}
-        onChange={() => {
-          console.log('edited');
-        }}
-      />
-      <CommentBox
-        isOwn={true}
-        isOnEdit={isOnEdit}
-        profileImages={{ png: JuliusProfilePNG, webp: JuliusProfileWEBP }}
-        commenter={'juliusomo'}
-        commentTimestamp={'2 days ago'}
-        comment={
-          "@ramsesmiron I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant."
-        }
-        upvoteValue={value}
-        onDeleteBtnClick={() => {
-          if (deleteModalRef.current && typeof deleteModalRef.current.showModal === 'function') {
-            deleteModalRef.current.showModal();
-          }
-        }}
-        onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
-        onMinusIconClick={() => setValue(value - 1)}
-        onPlusIconClick={() => setValue(value + 1)}
-        labelID={'comment-2'}
-        onChange={(e) => {
-          console.log(e.target.value);
-        }}
-        onUpdateBtnClick={(e) => {
-          e.preventDefault();
-          console.log(e);
-          setIsOnEdit(false);
+      {data.comments.map((c) => (
+        <>
+          <CommentBox
+            key={c.id}
+            comment={c.content}
+            commenter={c.user.username}
+            commentTimestamp={c.createdAt}
+            isOwn={c.user.username === data.currentUser.username}
+            isOnEdit={isOnEdit}
+            upvoteValue={c.score}
+            labelID={`comment-${c.user.username}-${c.id}`}
+            onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
+            onMinusIconClick={() => setValue(value - 1)}
+            onPlusIconClick={() => setValue(value + 1)}
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+            profileImages={{
+              png: require(`../assets/${c.user.image.png}`),
+              webp: require(`../assets/${c.user.image.webp}`),
+            }}
+          />
+          {c.replies.length !== 0 &&
+            c.replies.map((r) => (
+              <CommentBox
+                key={r.id}
+                comment={r.content}
+                commenter={r.user.username}
+                commentTimestamp={r.createdAt}
+                isOwn={r.user.username === data.currentUser.username}
+                isOnEdit={isOnEdit}
+                upvoteValue={r.score}
+                labelID={`comment-${r.user.username}-${r.id}`}
+                onDeleteBtnClick={() => deleteModalRef.current?.showModal()}
+                onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
+                onMinusIconClick={() => setValue(value - 1)}
+                onPlusIconClick={() => setValue(value + 1)}
+                onChange={(e) => {
+                  console.log(e.target.value);
+                }}
+                profileImages={{
+                  png: require(`../assets/${r.user.image.png}`),
+                  webp: require(`../assets/${r.user.image.webp}`),
+                }}
+              />
+            ))}
+        </>
+      ))}
+      <CommentInputForm
+        labelID="comment-5"
+        isReply={false}
+        onCommentChange={(e) => console.log(e.target.value)}
+        profileImages={{
+          png: require(`../assets/${data.currentUser.image.png}`),
+          webp: require(`../assets/${data.currentUser.image.webp}`),
         }}
       />
       <Modal
-        id="delete-modal"
         ref={deleteModalRef}
         onCancelBtnClick={() => deleteModalRef.current && deleteModalRef.current.close()}
         onDeleteBtnClick={() => console.log('comment deleted')}
-      />
-      <CommentInputForm
-        labelID="comment-3"
-        isReply={false}
-        onCommentChange={(e) => console.log(e)}
-        profileImages={{ png: JuliusProfilePNG, webp: JuliusProfileWEBP }}
-      />
-      <CommentInputForm
-        labelID="comment-4"
-        isReply={true}
-        replyingTo={'ramsesmiron'}
-        onCommentChange={(e) => console.log(e.target.value)}
-        profileImages={{ png: JuliusProfilePNG, webp: JuliusProfileWEBP }}
       />
     </Box>
   );
