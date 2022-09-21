96971f4534409956219da76a28e902b5c264604d
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-09-21 09:28:12 +0300

    Add functionality for comment input form, plus and minus button clicks

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,79 +1,194 @@
 import React from 'react';
 import { Box, CommentBox, CommentInputForm, Modal } from '../components';
-import data from '../data.json';
+import Data from '../data.json';
+import { IData } from './types';
 
 function Main() {
-  const [value, setValue] = React.useState(12);
   const [isOnEdit, setIsOnEdit] = React.useState(false);
   const deleteModalRef = React.useRef<HTMLDialogElement>(null);
+  const [data, setData] = React.useState<IData>({} as IData);
+  const [newComment, setNewComment] = React.useState<string>();
+  let id = 5;
 
   React.useEffect(() => {
-    console.log('mounted');
-    console.log(data);
+    if (localStorage.getItem('data')) {
+      setData(JSON.parse(localStorage.getItem('data') || ''));
+    } else {
+      setData(Data as IData);
+      localStorage.setItem('data', JSON.stringify(Data));
+    }
   }, []);
+
+  const onMinusIconClick = (id: number, rId?: number) => {
+    if (!rId) {
+      const updatedData =
+        data.comments &&
+        data.comments.map((c) => {
+          c.id === id && c.score--;
+          return c;
+        });
+
+      setData({ comments: updatedData, currentUser: data.currentUser });
+
+      return;
+    }
+
+    const updatedData =
+      data.comments &&
+      data.comments.map((c) => {
+        c.id === id &&
+          c.replies?.map((r) => {
+            r.id === rId && r.score--;
+          });
+        return c;
+      });
+
+    setData({ comments: updatedData, currentUser: data.currentUser });
+  };
+
+  const onPlusIconClick = (id: number, rId?: number) => {
+    if (!rId) {
+      const updatedData =
+        data.comments &&
+        data.comments.map((c) => {
+          c.id === id && c.score++;
+          return c;
+        });
+
+      setData({ comments: updatedData, currentUser: data.currentUser });
+
+      return;
+    }
+
+    const updatedData =
+      data.comments &&
+      data.comments.map((c) => {
+        c.id === id &&
+          c.replies?.map((r) => {
+            r.id === rId && r.score++;
+          });
+        return c;
+      });
+
+    setData({ comments: updatedData, currentUser: data.currentUser });
+  };
+
+  const onReplyBtnClick = (id: number, rId?: number) => {
+    console.log(id, rId);
+  };
+
+  const onSubmitBtnClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setData({
+      currentUser: data.currentUser,
+      comments: [
+        ...data.comments,
+        {
+          id,
+          content: newComment || '',
+          createdAt: 'few minutes ago',
+          score: 0,
+          user: data.currentUser,
+        },
+      ],
+    });
+    setNewComment('');
+    id++;
+  };
 
   return (
     <Box tag={'main'}>
-      {data.comments.map((c) => (
-        <>
-          <CommentBox
-            key={c.id}
-            comment={c.content}
-            commenter={c.user.username}
-            commentTimestamp={c.createdAt}
-            isOwn={c.user.username === data.currentUser.username}
-            isOnEdit={isOnEdit}
-            upvoteValue={c.score}
-            labelID={`comment-${c.user.username}-${c.id}`}
-            onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
-            onMinusIconClick={() => setValue(value - 1)}
-            onPlusIconClick={() => setValue(value + 1)}
-            onChange={(e) => {
-              console.log(e.target.value);
-            }}
-            profileImages={{
-              png: require(`../assets/${c.user.image.png}`),
-              webp: require(`../assets/${c.user.image.webp}`),
-            }}
-          />
-          {c.replies.length !== 0 && (
-            <Box tag={'div'} className="comment-box--indent">
-              {c.replies.map((r) => (
-                <CommentBox
-                  key={r.id}
-                  comment={`@${r.replyingTo} ${r.content}`}
-                  commenter={r.user.username}
-                  commentTimestamp={r.createdAt}
-                  isOwn={r.user.username === data.currentUser.username}
-                  isOnEdit={isOnEdit}
-                  upvoteValue={r.score}
-                  labelID={`comment-${r.user.username}-${r.id}`}
-                  onDeleteBtnClick={() => deleteModalRef.current?.showModal()}
-                  onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
-                  onMinusIconClick={() => setValue(value - 1)}
-                  onPlusIconClick={() => setValue(value + 1)}
-                  onChange={(e) => {
-                    console.log(e.target.value);
-                  }}
-                  profileImages={{
-                    png: require(`../assets/${r.user.image.png}`),
-                    webp: require(`../assets/${r.user.image.webp}`),
-                  }}
-                />
-              ))}
-            </Box>
-          )}
-        </>
-      ))}
-      <CommentInputForm
-        labelID="comment-5"
-        isReply={false}
-        onCommentChange={(e) => console.log(e.target.value)}
-        profileImages={{
-          png: require(`../assets/${data.currentUser.image.png}`),
-          webp: require(`../assets/${data.currentUser.image.webp}`),
-        }}
-      />
+      {data.comments &&
+        data.comments.length &&
+        data.comments.map((c) => (
+          <React.Fragment key={c.id}>
+            <CommentBox
+              data-id={c.id}
+              key={c.id}
+              comment={c.content}
+              commenter={c.user.username}
+              commentTimestamp={c.createdAt}
+              isOwn={c.user.username === data.currentUser?.username}
+              isOnEdit={isOnEdit}
+              upvoteValue={c.score}
+              labelID={`comment-${c.user.username}-${c.id}`}
+              onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
+              onMinusIconClick={() => onMinusIconClick(c.id)}
+              onPlusIconClick={() => onPlusIconClick(c.id)}
+              onReplyBtnClick={() => onReplyBtnClick(c.id)}
+              onChange={(e) => {
+                console.log(e.target.value);
+              }}
+              profileImages={{
+                png: require(`../assets/${c.user.image.png}`),
+                webp: require(`../assets/${c.user.image.webp}`),
+              }}
+            />
+            <CommentInputForm
+              labelID="comment-5"
+              isReply={true}
+              replyingTo={c.user.username}
+              onCommentChange={(e) => setNewComment(e.target.value)}
+              profileImages={{
+                png: require(`../assets/${data.currentUser?.image.png}`),
+                webp: require(`../assets/${data.currentUser?.image.webp}`),
+              }}
+            />
+            {c.replies && c.replies.length !== 0 && (
+              <Box tag={'div'} className="comment-box--indent">
+                {c.replies.map((r) => (
+                  <React.Fragment key={r.id}>
+                    <CommentBox
+                      key={r.id}
+                      comment={`@${r.replyingTo} ${r.content}`}
+                      commenter={r.user.username}
+                      commentTimestamp={r.createdAt}
+                      isOwn={r.user.username === data.currentUser?.username}
+                      isOnEdit={isOnEdit}
+                      upvoteValue={r.score}
+                      labelID={`comment-${r.user.username}-${r.id}`}
+                      onDeleteBtnClick={() => deleteModalRef.current?.showModal()}
+                      onEditBtnClick={() => setIsOnEdit(!isOnEdit)}
+                      onMinusIconClick={() => onMinusIconClick(c.id, r.id)}
+                      onPlusIconClick={() => onPlusIconClick(c.id, r.id)}
+                      onReplyBtnClick={() => onReplyBtnClick(c.id, r.id)}
+                      onChange={(e) => {
+                        console.log(e.target.value);
+                      }}
+                      profileImages={{
+                        png: require(`../assets/${r.user.image.png}`),
+                        webp: require(`../assets/${r.user.image.webp}`),
+                      }}
+                    />
+                    <CommentInputForm
+                      labelID="comment-5"
+                      isReply={true}
+                      replyingTo={r.user.username}
+                      onCommentChange={(e) => setNewComment(e.target.value)}
+                      profileImages={{
+                        png: require(`../assets/${data.currentUser?.image.png}`),
+                        webp: require(`../assets/${data.currentUser?.image.webp}`),
+                      }}
+                    />
+                  </React.Fragment>
+                ))}
+              </Box>
+            )}
+          </React.Fragment>
+        ))}
+      {data.currentUser && (
+        <CommentInputForm
+          labelID="comment-5"
+          isReply={false}
+          onCommentChange={(e) => setNewComment(e.target.value)}
+          profileImages={{
+            png: require(`../assets/${data.currentUser?.image.png}`),
+            webp: require(`../assets/${data.currentUser?.image.webp}`),
+          }}
+          onSubmitBtnClick={onSubmitBtnClick}
+          value={newComment}
+        />
+      )}
       <Modal
         ref={deleteModalRef}
         onCancelBtnClick={() => deleteModalRef.current && deleteModalRef.current.close()}
---
+++ src/pages/types.ts
@@ -0,0 +1,19 @@
+export interface IData {
+  currentUser: IUser;
+  comments: IComment[];
+}
+
+export interface IUser {
+  username: string;
+  image: { png: string; webp: string };
+}
+
+export interface IComment {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: IUser;
+  replies?: IComment[];
+  replyingTo?: string;
+}
