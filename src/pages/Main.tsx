73c4e5503d83fdc31bc824a256c78f686a99e55b
Yemisrach <se.yemisrach.serawit@gmail.com>
2022-10-11 20:12:43 +0300

    Utilize new recoil state in callbacks

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Box, Button, CommentBox, CommentInputForm, Heading, Modal } from '../components';
-import Data from '../data.json';
-import { extractUserName, selectTextAreaNode } from '../utils';
-import { IComment, IData } from '../types';
+import { useRecoilValue } from 'recoil';
 import { ReactComponent as SunIcon } from '../assets/icons/icon-sun.svg';
 import { ReactComponent as MoonIcon } from '../assets/icons/icon-moon.svg';
-import { ThemeContext } from '../App';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { Box, Button, CommentBox, CommentInputForm, Heading, Modal } from '../components';
+import { useStickystate } from '../hooks';
 import { commentsState, currentUserState } from '../store';
+import { IComment } from '../types';
+import { extractUserName, selectTextAreaNode } from '../utils';
+import { StringDictionary } from '../utils/constants';
+import { ThemeContext } from '../App';
 
 function Main() {
   const deleteModalRef = React.useRef<HTMLDialogElement>(null);
   const [initialId, setInitialId] = React.useState<number>(0);
   const [id, setId] = React.useState<number>(0);
-  const [data, setData] = React.useState<IData>(
-    ((localStorage.getItem('data') && JSON.parse(localStorage.getItem('data') as string)) ||
-      Data) as IData
+  const [comments, setComments] = useStickystate<IComment[]>(
+    commentsState,
+    StringDictionary.COMMENTS
   );
-  const [comments, setComments] = useRecoilState(commentsState);
   const currentUser = useRecoilValue(currentUserState);
   const [newComment, setNewComment] = React.useState<string>('');
   const [commentToDelete, setCommentToDelete] = React.useState<{ cId: number; rId?: number }>();
@@ -26,8 +26,8 @@ function Main() {
   const context = React.useContext(ThemeContext);
 
   React.useEffect(() => {
-    let totalComments = data.comments.length;
-    data.comments.forEach((c) => {
+    let totalComments = comments.length;
+    comments.forEach((c) => {
       totalComments += c.replies?.length || 0;
     });
     setInitialId(totalComments);
@@ -35,14 +35,29 @@ function Main() {
   }, []);
 
   React.useEffect(() => {
-    localStorage.setItem('data', JSON.stringify(data));
-  }, [data]);
-
-  React.useEffect(() => {
     if (currentTextAreaId && selectTextAreaNode(currentTextAreaId)) {
       selectTextAreaNode(currentTextAreaId).focus();
     }
   }, [currentTextAreaId]);
+
+  const changeState = (property: keyof IComment, value: unknown, cId: number, rId?: number) => {
+    if (!rId) {
+      setComments((currentValue) =>
+        currentValue.map((c) => (c.id === cId ? { ...c, [property]: value } : c))
+      );
+    } else {
+      setComments((currentValue) =>
+        currentValue.map((c) =>
+          c.id === cId
+            ? {
+                ...c,
+                replies: c.replies?.map((r) => (r.id === rId ? { ...r, [property]: value } : r)),
+              }
+            : c
+        )
+      );
+    }
+  };
 
   // Function for plus and minus button clicks
   const onMinusPlusIconClick = (isMinus: boolean, cId: number, rId?: number) => {
@@ -75,19 +90,15 @@ function Main() {
     // Don't submit if value is empty
     if (!newComment.trim()) return;
 
-    setData({
-      currentUser: data.currentUser,
-      comments: [
-        ...data.comments,
-        {
-          id,
-          content: newComment,
-          createdAt: '1 sec ago',
-          score: 0,
-          user: data.currentUser,
-        },
-      ],
-    });
+    setComments((currentValue) =>
+      currentValue.concat({
+        id,
+        content: newComment,
+        createdAt: '1 sec ago',
+        score: 0,
+        user: currentUser,
+      })
+    );
 
     setNewComment('');
     setId((id) => ++id);
@@ -96,26 +107,7 @@ function Main() {
   // Function for click on reply button in threads
   const onReplyBtnClick = (isReply: boolean, cId: number, rId?: number) => {
     isReply ? setCurrentTextAreaId(`comment-${id}-for-${rId || cId}`) : setCurrentTextAreaId(null);
-
-    let updatedData;
-    if (!rId) {
-      updatedData = data.comments.map((c) => {
-        if (c.id === cId) c.isOnReply = !c.isOnReply;
-        return c;
-      });
-    } else {
-      updatedData = data.comments.map((c) => {
-        c.id === cId &&
-          c.replies?.map((r) => {
-            if (r.id === rId) r.isOnReply = !r.isOnReply;
-            return r;
-          });
-
-        return c;
-      });
-    }
-
-    setData({ comments: updatedData, currentUser: data.currentUser });
+    changeState('isOnReply', isReply ? true : false, cId, rId);
   };
 
   // Function for click on new reply submit button in thread
@@ -126,63 +118,55 @@ function Main() {
     // Don't submit if value is empty
     if (!extractUserName(newReplyValue).text.trim()) return;
 
-    const reply = {
+    const reply: IComment = {
       id,
       content: extractUserName(newReplyValue).text.trim(),
       createdAt: '1 sec ago',
       score: 0,
-      user: data.currentUser,
+      user: currentUser,
     };
-    let updatedData;
     setCurrentTextAreaId(null);
 
     if (!rId) {
-      updatedData = data.comments.map((c) => {
-        if (c.id === cId) {
-          c.replies = [...(c.replies as IComment[]), { ...reply, replyingTo: c.user.username }];
-          c.isOnReply = false;
-        }
-        return c;
-      });
+      setComments((currentValue) =>
+        currentValue.map((c) =>
+          c.id === cId
+            ? {
+                ...c,
+                isOnReply: false,
+                replies: c.replies?.concat({ ...reply, replyingTo: c.user.username }),
+              }
+            : c
+        )
+      );
     } else {
-      updatedData = data.comments.map((c) => {
-        if (c.id === cId) {
-          c.replies?.map((r) => {
-            if (r.id === rId) {
-              c.replies = [...(c.replies as IComment[]), { ...reply, replyingTo: r.user.username }];
-              r.isOnReply = false;
-            }
-          });
-        }
-        return c;
-      });
+      setComments((currentValue) =>
+        currentValue.map((c) =>
+          c.id === cId
+            ? {
+                ...c,
+                replies: c.replies
+                  ?.map((r) => {
+                    if (r.id === rId) {
+                      reply.replyingTo = r.user.username;
+                      return { ...r, isOnReply: false };
+                    }
+                    return r;
+                  })
+                  .concat(reply),
+              }
+            : c
+        )
+      );
     }
 
-    setData({ currentUser: data.currentUser, comments: updatedData as IComment[] });
     setId((id) => ++id);
   };
 
   // Function for click on edit button on own comments
   const onEditBtnClick = (isEdit: boolean, cId: number, rId?: number) => {
     isEdit ? setCurrentTextAreaId(`comment-${rId || cId}`) : setCurrentTextAreaId(null);
-    let updatedData;
-    if (!rId) {
-      updatedData = data.comments.map((c) => {
-        if (c.id === cId) c.isOnEdit = !c.isOnEdit;
-        return c;
-      });
-    } else {
-      updatedData = data.comments.map((c) => {
-        c.id === cId &&
-          c.replies?.map((r) => {
-            if (r.id === rId) r.isOnEdit = !r.isOnEdit;
-            return r;
-          });
-        return c;
-      });
-    }
-
-    setData({ comments: updatedData, currentUser: data.currentUser });
+    changeState('isOnEdit', isEdit ? true : false, cId, rId);
   };
 
   // Function for update button click after editing comment
@@ -193,31 +177,10 @@ function Main() {
     // Don't submit if value is empty
     if (!extractUserName(updatingCommentValue).text.trim()) return;
 
-    let updatedData;
-    setCurrentTextAreaId(null);
-    if (!rId) {
-      updatedData = data.comments.map((c) => {
-        if (c.id === cId) {
-          c.content = extractUserName(updatingCommentValue).text.trim();
-          c.isOnEdit = false;
-        }
-        return c;
-      });
-    } else {
-      updatedData = data.comments.map((c) => {
-        c.id === cId &&
-          c.replies?.map((r) => {
-            if (r.id === rId) {
-              r.content = extractUserName(updatingCommentValue).text.trim();
-              r.isOnEdit = false;
-            }
-            return r;
-          });
-        return c;
-      });
-    }
+    changeState('content', extractUserName(updatingCommentValue).text.trim(), cId, rId);
+    changeState('isOnEdit', false, cId, rId);
 
-    setData({ comments: updatedData, currentUser: data.currentUser });
+    setCurrentTextAreaId(null);
   };
 
   const toggleOpenModal = () => {
@@ -233,18 +196,17 @@ function Main() {
 
   // Function for click on delete confirmation button on modal
   const onYesDeleteBtnClick = () => {
-    let updatedData;
     if (!commentToDelete?.rId) {
-      updatedData = data.comments.filter((c) => c.id !== commentToDelete?.cId);
+      setComments((currentValue) => currentValue.filter((c) => c.id !== commentToDelete?.cId));
     } else {
-      updatedData = data.comments.map((c) => {
-        if (c.id === commentToDelete?.cId)
-          c.replies = c.replies?.filter((r) => r.id !== commentToDelete.rId);
-        return c;
-      });
+      setComments((currentValue) =>
+        currentValue.map((c) =>
+          c.id === commentToDelete.cId
+            ? { ...c, replies: c.replies?.filter((r) => r.id !== commentToDelete.rId) }
+            : c
+        )
+      );
     }
-
-    setData({ comments: updatedData, currentUser: data.currentUser });
     toggleOpenModal();
   };
 
@@ -284,7 +246,7 @@ function Main() {
                 comment={c.content}
                 commenter={c.user.username}
                 commentTimestamp={c.createdAt}
-                isOwn={c.user.username === data.currentUser?.username}
+                isOwn={c.user.username === currentUser?.username}
                 onDeleteBtnClick={() => onDeleteBtnClick(c.id)}
                 isOnEdit={c.isOnEdit as boolean}
                 isOnReply={c.isOnReply as boolean}
@@ -307,11 +269,11 @@ function Main() {
                     isReply={true}
                     replyingTo={c.user.username}
                     profileImages={{
-                      png: require(`../assets/${data.currentUser?.image.png}`),
-                      webp: require(`../assets/${data.currentUser?.image.webp}`),
+                      png: require(`../assets/${currentUser?.image.png}`),
+                      webp: require(`../assets/${currentUser?.image.webp}`),
                     }}
                     onSubmitBtnClick={(e) => onReplySubmitBtnClick(e, c.id)}
-                    commenter={data.currentUser.username}
+                    commenter={currentUser.username}
                   />
                 )}
                 {c.replies &&
@@ -325,7 +287,7 @@ function Main() {
                         comment={`@${r.replyingTo} ${r.content}`}
                         commenter={r.user.username}
                         commentTimestamp={r.createdAt}
-                        isOwn={r.user.username === data.currentUser?.username}
+                        isOwn={r.user.username === currentUser?.username}
                         isOnEdit={r.isOnEdit as boolean}
                         isOnReply={r.isOnReply as boolean}
                         upvoteValue={r.score}
@@ -347,11 +309,11 @@ function Main() {
                           isReply={true}
                           replyingTo={r.user.username}
                           profileImages={{
-                            png: require(`../assets/${data.currentUser?.image.png}`),
-                            webp: require(`../assets/${data.currentUser?.image.webp}`),
+                            png: require(`../assets/${currentUser?.image.png}`),
+                            webp: require(`../assets/${currentUser?.image.webp}`),
                           }}
                           onSubmitBtnClick={(e) => onReplySubmitBtnClick(e, c.id, r.id)}
-                          commenter={data.currentUser.username}
+                          commenter={currentUser.username}
                         />
                       )}
                     </React.Fragment>
